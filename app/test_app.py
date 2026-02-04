198986a98cae9ff7ffcffbc294bcbfe674f4daff
Zhang Wen <wzhang@marinsoftware.com>
2026-02-04 12:37:58 +0800

    first version

---
+++ app/test_app.py
@@ -0,0 +1,14 @@
+import app
+import pytest
+
+def test_hello_endpoint():
+    client = app.app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Hello helm-prometheus-demo World!' in response.data
+
+def test_health_endpoint():
+    client = app.app.test_client()
+    response = client.get('/health')
+    assert response.status_code == 200
+    assert response.data == b'helm-prometheus-demo OK'
